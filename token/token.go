85cdbdffb6793f9f2fdced656fcde9c3ba279164
Duja03 <mdujanovic03@gmail.com>
2025-05-27 12:38:54 +0200

    added all keywords and remaining single letter tokens

--- token/token.go
+++ token/token.go
@@ -9,8 +9,15 @@ const (
 	INT   = "INT"   // 1343456
 
 	// Operators:
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// Delimiters:
 	COMMA     = ","
@@ -24,6 +31,11 @@ const (
 	// Keywords:
 	LET      = "LET"
 	FUNCTION = "FUNCTION"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 type TokenType string
@@ -34,8 +46,13 @@ type Token struct {
 }
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
