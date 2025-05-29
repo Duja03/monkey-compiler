2469546cf3fc9cfcc117f71280bc9fdcba96bcf2
Duja03 <mdujanovic03@gmail.com>
2025-05-29 10:32:12 +0200

    added error handling while evaluating

--- object/object.go
+++ object/object.go
@@ -8,6 +8,7 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	INTEGER_OBJ      = "INTEGER"
 	BOOLEAN_OBJ      = "BOOLEAN"
+	ERROR_OBJ        = "ERROR"
 	NULL_OBJ         = "NULL"
 )
 
@@ -41,3 +42,10 @@ type ReturnValue struct {
 
 func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
