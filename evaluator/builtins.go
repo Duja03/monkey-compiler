d0972b4d3bc33a68e92e698077100909e6887cda
Duja03 <mdujanovic03@gmail.com>
2025-05-30 10:30:55 +0200

    parsing hash literals

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -106,8 +106,21 @@ var builtins = map[string]*object.Builtin{
 			copy(newElements, arr.Elements)
 
 			newElements[length] = args[1]
-			
+
 			return &object.Array{Elements: newElements}
 		},
 	},
 }
+
+/*
+
+map function:
+let map = fn(arr, f) {let iter = fn(arr, accumulated) {if (len(arr) == 0) {accumulated} else {iter(rest(arr), push(accumulated, f(first(arr))));}}; iter(arr, []);};
+
+reduce function:
+let reduce = fn(arr, initial, f) {let iter = fn(arr, result) {if (len(arr) == 0) {result} else {iter(rest(arr), f(result, first(arr)));}};iter(arr, initial);};
+
+sum function:
+let sum = fn(arr) {reduce(arr, 0, fn(initial, el) { initial + el });};
+
+*/
